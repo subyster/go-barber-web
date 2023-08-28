bc1835f5995e8469633b5337e4fd393b84600c90
Arthur Amaral <arthur.amaral1@gmail.com>
2023-08-28 16:28:26 -0300

    Go barber web

---
+++ src/custom.d.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.JPG' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.PNG' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: any;
+  export default content;
+}
